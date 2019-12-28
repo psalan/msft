949a1c1f8a90a53af445ca450e2734c37a521676
psalan <psalan@psalan.com>
2019-12-28 12:58:02 +0100

    add the define_work function

--- msft_frame.py
+++ msft_frame.py
@@ -9,6 +9,9 @@ class Frame_msft():
         self.try_count = 0
 
     def get_work_file(self):
+        """Utasítást ad a munka fájl elhelyezésére,\
+            visszaadja munkafájlt és az útvonalát."""
+
         print('Másold az excel fájlt a "files_xlsx" könyvtárba!\
             \nMajd írd be a fájl nevét!')
 
@@ -28,34 +31,37 @@ class Frame_msft():
             return False
 
     def app_end(self):
-        print('kint') # majd törölni
+        """Kilép a programból."""
+        print('kint')  # majd törölni
         exit()
 
     def is_end(self):
+        """A kilépés lehetőségét ajánlja fel."""
         answer = str.lower(input('Kilépsz a programból?\
             \nIGEN\n'))
         if not (answer == 'n' or answer == 'nem'):
             print('Ok.\nVisszlát!')
             self.app_end()
 
+    def define_work(self):
+        """Bekéri a munka fájlt, és gondoskodik az ellenőrzéséről."""
+        done = False
+        while True:
+            if self.try_count > 1:
+                self.is_end()
+            filename = self.get_work_file()
+            if self.is_valid_filename(filename):
+                done = True
+                return done
+            self.try_count += 1
+            if self.try_count == 5:
+                self.app_end()
+
     def start(self):
         """Elkezdi a keret a program felépítését."""
-        if (ftbann.make_banner(ftbann.banner)):
-
-            while True:
-                if self.try_count > 1:
-                    self.is_end()
-                filename = self.get_work_file()
-                if self.is_valid_filename(filename):
-                    break
-                self.try_count += 1
-
-            print('Itt lesz a "mag"')
-
-        else:
+        if not (ftbann.make_banner(ftbann.banner)):
             self.app_end()
-
-
-if __name__ == "__main__":
-    starter = Frame_msft()
-    starter.start()
+        else:
+            if self.define_work():
+                print('Itt lesz a "mag"')
+                self.app_end()
