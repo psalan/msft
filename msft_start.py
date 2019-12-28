05a56139445b838f23a1e6d78d2369617d809633
psalan <psalan@psalan.com>
2019-12-28 12:55:04 +0100

    add the start function

--- msft_start.py
+++ msft_start.py
@@ -1,4 +1,12 @@
 from msft_frame import Frame_msft
 
-starter = Frame_msft()
-starter.start()
+
+def ft_start():
+    """Egyetlen feladata, hogy elindítsa a\
+    MÁV-Start forda ellenőrző programot."""
+    starter = Frame_msft()
+    starter.start()
+
+
+if __name__ == "__main__":
+    ft_start()
