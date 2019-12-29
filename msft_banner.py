e68ea442ccbb508d704764795e0e769912541a4d
psalan <psalan@psalan.com>
2019-12-29 14:55:15 +0100

    update banner file

--- msft_banner.py
+++ msft_banner.py
@@ -2,7 +2,7 @@
 wellcome_str = 'Üdvözöl a  Máv-Start Forda Ellenőrző program!'
 
 # main_version.calendar.daily_relase
-version = 'ver: 0.191227.1'
+version = 'ver: 0.191229.1'
 
 # rows
 full_row = ['{0:#^55}', '\n', ]
