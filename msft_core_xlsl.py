c10326fe0e6d5202f0fdda5ec6d8dfca1ba7323c
psalan <psalan@psalan.com>
2019-12-29 15:04:46 +0100

    add core file

---
+++ msft_core_xlsl.py
@@ -0,0 +1,54 @@
+import openpyxl
+
+
+class Core_msft():
+    """Egy excel fájl feldolgozását segítő osztály."""
+
+    def __init__(self, filename):
+
+        # Konstansok
+        self.separate = 3
+        self.SIMPLE_CELL = "<class 'openpyxl.cell.cell.Cell'>"
+        self.MERGED_CELL = "<class 'openpyxl.cell.cell.MergedCell'>"
+
+        # Munkafájl
+        self.work_file = filename
+
+        # Munkafüzet
+        self.wb = openpyxl.load_workbook(self.work_file)
+
+        # Munkalapok
+        self.ws = self.wb.worksheets
+
+        # Munkalapok neve
+        self.ws_names = self.wb.sheetnames
+
+        # sor hossza, ennyi oszlopból áll
+        #
+
+    def is_simple_cell(self, cell):
+        if str(type(cell)) == self.SIMPLE_CELL:
+            return True
+        return False
+
+    def is_merged_cell(self, cell):
+        if str(type(cell)) == self.MERGED_CELL:
+            return True
+        return False
+
+    def ws_wide(self):
+        """ Visszatér a munkalap egy adott sorának utolsó\
+            egyesített cellájához tartozó oszlop számával. """
+        count = 1
+        col_id = 1
+        find_simple_cell = self.separate
+        # addig keres, amíg 3db nem egyesített cellát talál egymás után
+        while find_simple_cell != 0:
+            examine_cell = self.ws[0].cell(1, count)
+            if self.is_simple_cell(examine_cell):
+                find_simple_cell -= 1
+            if self.is_merged_cell(examine_cell):
+                col_id = count
+                find_simple_cell = self.separate
+            count += 1
+        return col_id
