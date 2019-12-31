41b9b45199b2d06b370196fe410e388aad892ed5
psalan <psalan@psalan.com>
2019-12-31 15:33:15 +0100

    add new attributes

--- msft_core_xlsl.py
+++ msft_core_xlsl.py
@@ -24,6 +24,22 @@ class Core_msft:
         # Munkalapok neve
         self.ws_names = self.wb.sheetnames
 
+        # Aktív kutatás
+        self.active_research = {'ws_poz': None,
+                                'ws_name': None,
+                                'ws_done': False,
+                                'ch_entry_point': None, 
+                                'ch_end_point': None,
+                                'ch_is_last': None,}
+
+        # Elöző kutatás
+        self.previous_research = {'ws_poz': None,
+                                'ws_name': None,
+                                'ws_done': False,
+                                'ch_entry_point': None, 
+                                'ch_end_point': 0,
+                                'ch_is_last': None,}                                
+
         # sor hossza, ennyi oszlopból áll
         #
 
@@ -36,6 +52,12 @@ class Core_msft:
         if str(type(cell)) == self.MERGED_CELL:
             return True
         return False
+
+    def set_active_ws(self, num: int):
+        """ Beállítja az aktív munkalap értékeit. """
+        self.active_research['ws_poz'] = num
+        self.active_research['ws_name'] = self.ws_names[num]
+
 
     def ws_wide(self):
         """ Visszatér a munkalap egy adott sorának utolsó\
@@ -59,12 +81,17 @@ class Core_msft:
         """ A excel munkalapon a szakaszokra bontott\
             Szolgálatok kezdő és végpontjait kutatja fel. """
 
-        def __init__(self, entry_point=1):
+        def __init__(self, active_research: set):
+            """ A szolgálatok leírását tartalmazó szakaszok keresését\
+                valósítja meg. """
             pass
 
-    def core_auto_execution(self):
-        """ Az excel fájl automatikus feldolgozási\
-            folyamatát végző füügvény."""
+    def core_main(self):
+        """ Az excel fájl automatikus feldolgozását végző függvény."""
 
         ftinf.get_str(ftinf.core['start'])
         ftinf.get_str_format(ftinf.core['ws_num'], len(self.ws_names))
+        for i in range(len(self.ws_names)):
+            self.set_active_ws(i)
+
+            print(self.active_research)
