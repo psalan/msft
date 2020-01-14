accd18701f8f12edd3364a5a84007015c51850a9
psalan <psalan@psalan.com>
2020-01-14 14:34:05 +0100

    set header data dictionaries

--- msft_patterns.py
+++ msft_patterns.py
@@ -1,4 +1,9 @@
-header = {}
-header_human_readable = {}
+head = {
+    'Serv_id_data': (1, 31), 'Serv_time_data': (5, 15),
+    'Worktime_data': (7, 15), 'Driver_time_data': (9, 15),
+    'MKSZ_data': (11, 15), }
+head_human_readable = {
+    'Serv_id_data': 'AE1', 'Serv_time_data': 'O5', 'Worktime_data': 'O7',
+    'Driver_time_data': 'O9', 'MKSZ_data': 'O11', }
 data_head = {}
 data_head_human_readable = {}
